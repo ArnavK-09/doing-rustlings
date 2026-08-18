9094c508e45d5565f0c4f9fb0e1fff55913a5d1b
ArnavK-09 <arnavkaushal09@gmail.com>
2026-08-18 20:51:55 +0530

    Doing Rustlings

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_uppercase().to_string() + chars.as_str(),
     }
 }
 
@@ -15,18 +15,19 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.iter().map(|w| capitalize_first(w)).collect()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    let s = words.iter().map(|w| capitalize_first(w)).collect();
+    s
 }
 
 fn main() {
-    // You can optionally experiment here.
+    dbg!(capitalize_words_vector(&["Hello", "World"]));
 }
 
 #[cfg(test)]
